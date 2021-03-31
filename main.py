74878d7d5e791bd55428bd0d1f9c8d7800908293
anushirahatti <aniruddhashirahatti@yahoo.com>
2021-03-31 09:52:10 +0000

    adding functionality to get stations based on query criteria

--- main.py
+++ main.py
@@ -22,34 +22,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return "<h1><center>Welcome to Solar App API!</center></h1>"
-
-
-
-@app.route('/populate')
-def populate():
-
-    url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?limit=1000"
-    token = "ylPeWbpuHSsbXHmtqurCJXfejdryavRe"
-    headers = {
-        'token': token,
-        'Content-Type': 'application/json; charset=utf-8'
-    }
-    
-    response = requests.get(url, headers=headers).text
-    response_info = json.loads(response)
-
-    # Initialize Firestore DB
-    cred = credentials.Certificate('key.json')
-    firebase_admin.initialize_app(cred)
-    db = firestore.client()
-
-    for result in response_info['results']:
-        doc_id = str(uuid.uuid4().hex)
-        db.collection(u'stations').document(u'{}'.format(doc_id)).set(result)
-
-
-    return response_info['metadata']
+    return "<center><h1>Welcome to Solar App API!</h1> <br/><br/> <h3><strong><p>Here are the options:</p> <ul><li>/stations</li></ul></strong></h3></center>"
 
 
 
@@ -63,49 +36,104 @@ def getStations():
         lat = '47.59163'
         print(lat)
         #lng = request.get_json().get('lng')
-        lng = '-122.1549'
+        lng = '-120.1549'
         print(lng)
         #start = request.get_json().get('start')
-        start = '1970-10-03'
+        start = '2018-10-03'
         print(start)
         #end = request.get_json().get('end')
-        end = '2015-09-10'
+        end = '2019-09-10'
         print(end)
+
 
         # convert latitude and longitude to FIPS to set locationid for filtering stations        
         key = "17953106d8134918b9ffdd624065750a"
         geocoder = OpenCageGeocode(key)
         results = geocoder.reverse_geocode(lat, lng)
-        fips = results[0]['annotations']['FIPS']['county']
+        country_code = results[0]['components']['country_code']
+        fips = ''
+        if country_code == 'us':
+            fips = results[0]['annotations']['FIPS']['county']
+        else:
+            return jsonify({"Info": "Not a location within United States. Please choose location within the United States."})
 
-        
-        # set the user parameters to NCDC URL and get filtered results
-        url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?limit=2&locationid=FIPS:{}&startdate={}&enddate={}".format(fips, start, end)
-        #url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations/COOP:010125"
-        token = "ylPeWbpuHSsbXHmtqurCJXfejdryavRe"
-        headers = {
-            'token': token,
-            'Content-Type': 'application/json; charset=utf-8'
-        }
-    
-        response = requests.get(url, headers=headers).text
-        response_info = json.loads(response)
 
-        # Initialize Firestore DB
+        # Initialize Firestore DB if not already initialized
         cred = credentials.Certificate('key.json')
-        firebase_admin.initialize_app(cred)
+
+        if not firebase_admin._apps:
+            firebase_admin.initialize_app(cred)
+        
         db = firestore.client()
 
-        # store retrieved data in firestore
-        for result in response_info['results']:
+        # construct query to check if the data exists in the database
+        query_ref = db.collection(u'queries').where(u'latitude', u'==', u'{}'.format(lat)).where(u'longitude', u'==', u'{}'.format(lng)).where(u'fips', u'==', u'{}'.format(fips)).where(u'startDate', u'==', u'{}'.format(start)).where(u'endDate', u'==', u'{}'.format(end))
+        docs = query_ref.stream()
+
+        # check length of docs
+        listSize = len(list(docs))
+        print(listSize)      
+
+        # if data doesn't exist in database, fetch data from API, store in database and return the query results
+        if listSize == 0:
+
+            # set the user parameters to NCDC URL and get filtered results
+            url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?limit=1000&locationid=FIPS:{}&startdate={}&enddate={}".format(fips, start, end)
+            #url = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations/COOP:010125"
+            token = "ylPeWbpuHSsbXHmtqurCJXfejdryavRe"
+            headers = {
+                'token': token,
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+    
+            response = requests.get(url, headers=headers).text
+            response_info = json.loads(response)
+            
+            if len(response_info) == 0:
+                return jsonify({"Info": "No results available to match the query. Please submit modified query."})
+
+            # store retrieved data in firestore
+            # create a new doc entry for the user query in firebase
             doc_id = str(uuid.uuid4().hex)
-            db.collection(u'stations').document(u'{}'.format(doc_id)).set(result)        
+
+            doc_ref = db.collection(u'queries').document(u'{}'.format(doc_id))
+            doc_ref.set({
+                u'id': u'{}'.format(doc_id),
+                u'latitude': u'{}'.format(lat),
+                u'longitude': u'{}'.format(lng),
+                u'fips': u'{}'.format(fips),
+                u'startDate': u'{}'.format(start),
+                u'endDate': u'{}'.format(end),
+                u'extent': u'',
+                u'dataCoverage': u'',
+                u'results': response_info['results'],
+                u'resultsCount': response_info['metadata']['resultset']['count'] 
+            })
+
+            return jsonify({"results": response_info['results']}), 201
 
 
-        return response_info['metadata'], 201
+        # if data exists, return the query results from database    
+        else: 
+
+            docId = ''
+
+            docs = query_ref.stream()
+            for doc in docs:
+                docId = doc.id
+
+            doc_rf = db.collection(u'queries').document(u'{}'.format(docId))
+
+            doc = doc_rf.get()
+            if doc.exists:
+                response_info = doc.to_dict()
+                return jsonify({"results": response_info['results']}), 201
+            else:
+                return jsonify({"results": []})
+ 
 
     else:
-        return jsonify({"data": "Data Uploaded"})
+        return jsonify({"data": "Data Uploaded"}), 201
 
 
 # Create a Product
