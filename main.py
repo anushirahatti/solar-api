cd875f665c692a7aeff0c40fcd4dd8404d35e4f0
anushirahatti <aniruddhashirahatti@yahoo.com>
2021-04-01 05:26:53 +0000

    adding functionality to get stations based on query criteria

--- main.py
+++ main.py
@@ -147,61 +147,7 @@ def getStations():
         return jsonify({"data": "Data Uploaded"}), 200
 
 
-# Create a Product
-@app.route('/product', methods=['POST'])
-def add_product():
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
 
-  new_product = Product(name, description, price, qty)
-
-  db.session.add(new_product)
-  db.session.commit()
-
-  return product_schema.jsonify(new_product)
-
-# Get All Products
-@app.route('/product', methods=['GET'])
-def get_products():
-  all_products = Product.query.all()
-  result = products_schema.dump(all_products)
-  return jsonify(result.data)
-
-# Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-  product = Product.query.get(id)
-  return product_schema.jsonify(product)
-
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-  product = Product.query.get(id)
-
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
-
-  product.name = name
-  product.description = description
-  product.price = price
-  product.qty = qty
-
-  db.session.commit()
-
-  return product_schema.jsonify(product)
-
-# Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-  product = Product.query.get(id)
-  db.session.delete(product)
-  db.session.commit()
-
-  return product_schema.jsonify(product)
 
 
 if __name__ == '__main__':
